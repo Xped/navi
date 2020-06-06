ca81324fcf649e50da13294f064b241c09b3578d
Xped <Xped@users.noreply.github.com>
2020-06-06 11:08:50 +0800

    Update icbc.php

--- icbc.php
+++ icbc.php
@@ -5,8 +5,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1", maximum-scale=1, user-scalable=no">
     <title>ICBC Redirect</title>
-                                                                                                           
-                                                                                                           《body》
+                                                                                                           </head>
+                                                                                                           <body>
 <a href="https://elife.icbc.com.cn/OFSTCARDWEB/dist/#/apply/index/null/null/null/null/EW0004600000000TJ12/null/null/460000608">ICBC Redirect</a>
                                                                                                                               
                                                                                                                               
